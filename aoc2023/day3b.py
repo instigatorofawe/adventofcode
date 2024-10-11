f11e0d79abb0ac508bb193a8164e953cf0e1bc20
Ran Liu <xabsox@gmail.com>
2024-10-11 01:18:47 -0700

    Day 3

--- aoc2023/day3b.py
+++ aoc2023/day3b.py
@@ -2,12 +2,13 @@ import sys
 
 
 def run():
-    symbols = {"*", "#", "$", "+", "-", "=", "@", "&", "%", "/"}
+    symbols = {"*"}
 
     symbol_table: list[list[int]] = []  # row, column
     number_table: list[list[tuple[int, int, int]]] = []  # row, value, start, end
+    gears: dict[tuple[int, int], list[int]] = {}
 
-    for line in sys.stdin:
+    for row, line in enumerate(sys.stdin):
         current_symbols: list[int] = []
         current_numbers: list[tuple[int, int, int]] = []
 
@@ -26,6 +27,7 @@ def run():
                     current_value = 0
                 if c in symbols:
                     current_symbols.append(i)
+                    gears[(row, i)] = []
 
         if current_start is not None:
             current_numbers.append((current_value, current_start, len(line) - 1))
@@ -33,27 +35,27 @@ def run():
         symbol_table.append(current_symbols)
         number_table.append(current_numbers)
 
-    total = 0
-
     for i, number_row in enumerate(number_table):
-        if i == 0:
-            symbol_indices = [
-                x for y in [symbol_table[i], symbol_table[i + 1]] for x in y
-            ]
-        elif i == len(number_table) - 1:
-            symbol_indices = [
-                x for y in [symbol_table[i - 1], symbol_table[i]] for x in y
-            ]
-        else:
-            symbol_indices = [
-                x
-                for y in [symbol_table[i - 1], symbol_table[i], symbol_table[i + 1]]
-                for x in y
-            ]
-
         for value, start, end in number_row:
-            if any([start - 1 <= x <= end + 1 for x in symbol_indices]):
-                total += value
+            if i > 0:
+                for j in symbol_table[i - 1]:
+                    if start - 1 <= j <= end + 1:
+                        gears[(i - 1, j)].append(value)
+            if i < len(number_table) - 1:
+
+                for j in symbol_table[i + 1]:
+                    if start - 1 <= j <= end + 1:
+                        gears[(i + 1, j)].append(value)
+
+            for j in symbol_table[i]:
+                if start - 1 <= j <= end + 1:
+                    gears[(i, j)].append(value)
+
+    valid_gears = [x for x in gears.values() if len(x) == 2]
+
+    total = 0
+    for a, b in valid_gears:
+        total += a * b
 
     print(total)
 
