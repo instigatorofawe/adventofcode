80e3121e1a5777df82c350984324416b534e09b4
Ran Liu <xabsox@gmail.com>
2024-10-11 00:08:44 -0700

    Day 2 in python

---
+++ aoc2023/day2b.py
@@ -0,0 +1,29 @@
+import sys
+
+
+def run():
+    power_sum = 0
+    for line in sys.stdin:
+        split_line = line.split(":")
+        draws = [
+            [y.strip().split(" ") for y in x.split(",")]
+            for x in split_line[1].split(";")
+        ]
+        draws_flat = [x for y in draws for x in y]
+
+        minimum = {"red": 0, "green": 0, "blue": 0}
+        for count, color in draws_flat:
+            minimum[color] = max(minimum[color], int(count))
+
+        prod = 1
+        for val in minimum.values():
+            prod *= val
+
+        power_sum += prod
+
+    print(power_sum)
+
+
+if __name__ == "__main__":
+
+    run()
