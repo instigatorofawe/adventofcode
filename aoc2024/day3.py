42f7e28f7adeb52dd3eff9a26cbbc07296116a81
Ran Liu <xabsox@gmail.com>
2024-12-03 13:11:51 +0800

    Add day 3, 2024

--- aoc2024/day3.py
+++ aoc2024/day3.py
@@ -2,15 +2,40 @@ import sys
 
 
 def part_a(lines: list[str]):
-    pass
+    result = 0
+    for line in lines:
+        for i in range(len(line)):
+            x = line[i:]
+            if x.startswith("mul(") and ")" in x:
+                exp = x[(x.index("(") + 1) : x.index(")")]
+                if "," in exp:
+                    s = exp.split(",")
+                    if s[0].isnumeric() and s[1].isnumeric():
+                        result += int(s[0]) * int(s[1])
+    print(result)
 
 
 def part_b(lines: list[str]):
-    pass
+    result = 0
+    enabled = True
+    for line in lines:
+        for i in range(len(line)):
+            x = line[i:]
+            if x.startswith("do()"):
+                enabled = True
+            elif x.startswith("don't()"):
+                enabled = False
+            elif enabled and x.startswith("mul(") and ")" in x:
+                exp = x[(x.index("(") + 1) : x.index(")")]
+                if "," in exp:
+                    s = exp.split(",")
+                    if s[0].isnumeric() and s[1].isnumeric():
+                        result += int(s[0]) * int(s[1])
+    print(result)
 
 
 def run():
-    lines = [line for line in sys.stdin]
+    lines = [line.strip() for line in sys.stdin]
     part_a(lines)
     part_b(lines)
 
